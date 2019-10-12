3077bfa40a7b9226440d26fd68795c73fb93a48d
Enio <enio_eon@hotmail.com>
2019-10-12 16:04:10 -0300

    Add reference to configurationManager and LogEventLeve

--- src/Flogging.core/Flogger.cs
+++ src/Flogging.core/Flogger.cs
@@ -1,6 +1,8 @@
 ﻿using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
