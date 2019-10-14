1865bd88718e02315bcc74e231b16bf68ff6d13b
Enio <enio_eon@hotmail.com>
2019-10-13 23:02:55 -0300

    Create a Dapper Extensions Method

---
+++ src/Flogging.Data/CustomDapper/DapperExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flogging.Data.CustomDapper
+{
+    public static class DapperExtensions
+    {
+        public static int DapperProcNonQuery(this IDbConnection db, string procName, object paramList = null,
+            IDbTransaction trans = null, int? timeoutsecond = null)
+        {
+            
+            try
+            {
+                return db.Execute(procName, paramList, trans, timeoutsecond, CommandType.StoredProcedure);
+            }
+            catch (Exception orig)
+            {
+                var ex = new Exception($"Dapper proc execution falled",orig);
+                AddDetailsToExcepetion(ex, procName, paramList);
+                throw ex;
+            }
+        }
+
+        private static void AddDetailsToExcepetion(Exception ex, string procName, object paramList)
+        {
+            ex.Data.Add("Procedure", procName);
+            if(paramList is DynamicParameters dynamicParams)
+            {
+                foreach (var p in dynamicParams.ParameterNames)
+                {
+                    ex.Data.Add(p, dynamicParams.Get<object>(p).ToString());
+                }
+            }
+            else
+            {
+                var props = paramList.GetType().GetProperties();
+                foreach (var prop in props)
+                {
+                    ex.Data.Add(prop.Name, prop.GetValue(paramList).ToString());
+                }
+
+            }
+        }
+    }
+}
